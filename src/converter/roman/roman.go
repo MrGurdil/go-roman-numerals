7c6dec2849499f5f8869ff94c97136554a57df7a
Unknown <mrgurdil@gmx.fr>
2018-05-11 14:53:07 +0200

    Implement convert(4) and its test
    refactored by adding a loop for numbers smaller than 4.

--- src/converter/roman/roman.go
+++ src/converter/roman/roman.go
@@ -9,12 +9,15 @@ func init() {
 // Convert an integer to it's roman numeral equivalent
 func Convert(a int) string {
 
-	ret := "III"
+	var ret string
 
-	if a == 1 {
-		ret = "I"
-	} else if a == 2 {
-		ret = "II"
+	if a == 4 {
+		ret = "IV"
+	} else if a < 4 {
+		for a > 0 {
+			ret += "I"
+			a--
+		}
 	}
 
 	return ret
