eebbb30961f1b078d6d86d80d91f031164e6a8e2
Unknown <mrgurdil@gmx.fr>
2018-05-11 11:30:54 +0200

    Minor improvements
    Separated the test functions. Added support for converting the number 2.

--- src/converter/roman/roman.go
+++ src/converter/roman/roman.go
@@ -8,5 +8,13 @@ func init() {
 
 // Convert an integer to it's roman numeral equivalent
 func Convert(a int) string {
-	return "I"
+
+	ret := "II"
+
+	if a == 1 {
+		ret = "I"
+	}
+
+	return ret
+
 }
--- src/converter/romantests/roman_test.go
+++ src/converter/romantests/roman_test.go
@@ -7,14 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestRomanConverter is the main test function for the converter/roman package
-func TestRomanConverter(t *testing.T) {
+// Test naming convention : Test{Func name}_{Given}_{Returns}(t *testing.T)
 
-	// Test that Convert(1) returns I
+// TestConvert_1_I Given a parameter of 1 roman.Convert() returns "I"
+func TestConvert_1_I(t *testing.T) {
+
 	var convertedNumber1 = roman.Convert(1)
 	assert.Equal(t, "I", convertedNumber1, "1 = I")
+}
 
-	// Test that Convert(2) returns II
+// TestConvert_2_II Given a parameter of 2, roman.Convert() returns "II"
+func TestConvert_2_II(t *testing.T) {
+
 	var convertedNumber2 = roman.Convert(2)
 	assert.Equal(t, "II", convertedNumber2, "2 = II")
 }
